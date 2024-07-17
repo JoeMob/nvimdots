53dbb92de42b6f4aca4c30ecf2fa7ef2f95be7f3
JoeMob <wangyuxiaojoemob@outlook.com>
2024-07-17 16:22:12 +0800

    Add user custom.

---
+++ lua/user/configs/nvim-tree.lua
@@ -0,0 +1,16 @@
+return {
+	view = {
+		signcolumn = "no",
+		float = {
+			enable = true,
+			open_win_config = {
+				relative = "editor",
+				border = "rounded",
+				width = 32,
+				height = 32,
+				row = 1,
+				col = 1,
+			},
+		},
+	},
+}
