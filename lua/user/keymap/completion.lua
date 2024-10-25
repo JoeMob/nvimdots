20984d03abd97329c22297269b9a77e5f16c80c9
joemob <wangyuxiaojoemob@outlook.com>
2024-10-25 11:49:16 +0800

    Change format keymap to global scope.

--- lua/user/keymap/completion.lua
+++ lua/user/keymap/completion.lua
@@ -6,7 +6,27 @@ local M = require("modules.configs.completion.formatting")
 local opts = { pattern = "*", timeout = require("core.settings").format_timeout }
 
 -- Place global keymaps here.
-mappings["plug_map"] = {}
+mappings["plug_map"] = {
+	["n|<A-S-f>"] = map_callback(function()
+			M.format({
+				timeout_ms = opts.timeout,
+				filter = M.format_filter,
+			})
+		end)
+		:with_noremap()
+		:with_silent()
+		:with_desc("formatter: Format code"),
+	["i|<A-S-f>"] = map_callback(function()
+			vim.api.nvim_input("<Esc>l")
+			M.format({
+				timeout_ms = opts.timeout,
+				filter = M.format_filter,
+			})
+		end)
+		:with_noremap()
+		:with_silent()
+		:with_desc("formatter: Format code"),
+}
 
 -- NOTE: This function is special! Keymaps defined here are ONLY effective in buffers with LSP(s) attached
 -- NOTE: Make sure to include `:with_buffer(buf)` to limit the scope of your mappings.
@@ -15,25 +35,6 @@ mappings["lsp"] = function(buf)
 	return {
 		-- Example
 		["n|K"] = map_cr("Lspsaga hover_doc"):with_buffer(buf):with_desc("lsp: Show doc"),
-		["n|<A-S-f>"] = map_callback(function()
-				M.format({
-					timeout_ms = opts.timeout,
-					filter = M.format_filter,
-				})
-			end)
-			:with_noremap()
-			:with_silent()
-			:with_desc("formatter: Format code"),
-		["i|<A-S-f>"] = map_callback(function()
-				vim.api.nvim_input("<Esc>l")
-				M.format({
-					timeout_ms = opts.timeout,
-					filter = M.format_filter,
-				})
-			end)
-			:with_noremap()
-			:with_silent()
-			:with_desc("formatter: Format code"),
 	}
 end
 
