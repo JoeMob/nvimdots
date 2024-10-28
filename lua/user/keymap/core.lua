0ce84cba617a4ec2b2df0e11bd077cf6c513c26d
joemob <wangyuxiaojoemob@outlook.com>
2024-10-28 15:49:36 +0800

    Remove ignored files

--- lua/user/keymap/core.lua
+++
@@ -1,8 +0,0 @@
-local bind = require("keymap.bind")
-local map_cmd = bind.map_cmd
-return {
-	["n|<A-h>"] = map_cmd("zh"):with_noremap():with_desc("edit: Next search result"),
-	["n|<A-l>"] = map_cmd("zl"):with_noremap():with_desc("edit: Next search result"),
-	["n|<A-S-h>"] = map_cmd("zH"):with_noremap():with_desc("edit: Next search result"),
-	["n|<A-S-l>"] = map_cmd("zL"):with_noremap():with_desc("edit: Next search result"),
-}
