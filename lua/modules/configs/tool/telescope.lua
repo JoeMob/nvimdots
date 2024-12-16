60a71b83c9ca4fd79d6d871a845465d5c1d00ebc
joemob <wangyuxiaojoemob@outlook.com>
2024-12-16 14:54:18 +0800

    Telescope live_grep no ignore.

--- lua/modules/configs/tool/telescope.lua
+++ lua/modules/configs/tool/telescope.lua
@@ -11,6 +11,7 @@ return function()
 				"--line-number",
 				"--column",
 				"--smart-case",
+				"--no-ignore",
 			},
 			initial_mode = "insert",
 			prompt_prefix = " " .. icons.ui.Telescope .. " ",
